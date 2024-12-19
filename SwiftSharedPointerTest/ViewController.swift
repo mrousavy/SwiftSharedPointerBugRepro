2cc832acbcc0a568340c7d8a45a07790a1631a97
Marc Rousavy <me@mrousavy.com>
2024-12-19 14:11:29 +0100

    Initial commit

---
+++ SwiftSharedPointerTest/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SwiftSharedPointerTest
+//
+//  Created by Marc Rousavy on 19.12.24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
